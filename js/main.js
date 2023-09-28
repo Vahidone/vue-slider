5b0cc9faefb436ddff1ddac73cfb6cbc8bdd65f5
Vahidone <v.ban.roma@gmail.com>
2023-09-28 18:19:25 +0200

    feat: add function for btn prev/next

--- js/main.js
+++ js/main.js
@@ -1,39 +1,51 @@
+const {createApp} = Vue;
 
-const {createdApp} = Vue;
-
-createdApp ({
-  data(){
+createApp({
+  data() {
     return {
       photos: [
         {
           image: 'img/01.webp',
           title: 'Marvel\'s Spiderman Miles Morale',
           text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-        },
-        {
+      }, {
           image: 'img/02.webp',
           title: 'Ratchet & Clank: Rift Apart',
           text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-        },
-        {
+      }, {
           image: 'img/03.webp',
           title: 'Fortnite',
           text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-        },
-        {
+      }, {
           image: 'img/04.webp',
           title: 'Stray',
           text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-        },
-        {
+      }, {
           image: 'img/05.webp',
           title: "Marvel's Avengers",
           text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-        }
+      }
       ],
+      counterImg: 0,
     }
   },
 
-}).mount('#app');
 
-console.log('ciao vue');
+  
+  methods: {
+    btnNextPrev(isNext){
+      isNext ? this.counterImg++ : this.counterImg--;
+
+      if (this.counterImg === this.photos.length){
+        this.counterImg = 0;
+      }else if(this.counterImg < 0){
+        this.counterImg = this.photos.length - 1;
+      }
+    }
+    
+  },
+  
+
+
+
+}).mount("#app")
